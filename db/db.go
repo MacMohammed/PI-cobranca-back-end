66d359b5257ab8cdb6b66a92db6ae265db7d8d55
MacMohammed <macmohammed@gmail.com>
2022-04-13 07:09:16 -0300

    Correção na string de conexao

--- db/db.go
+++ db/db.go
@@ -8,8 +8,8 @@ import (
 
 //Função para Conectar ao Banco de Dados Postgres
 func ConectBD() *sql.DB {
-	connectino := "user=gebeewwzjtnuar dbname=dap7h89poiespl password=d49af27e6b6de17aaf323d7839547b58fe4b8a9296faa8f5f9d149931b229068 host=ec2-44-194-4-127.compute-1.amazonaws.com sslmode=disable"
-	db, err := sql.Open("postgres", connectino)
+	strconnection := "user=nknpxencptienk dbname=d7l4rvnlb3opsu password=4916af49ef0d66da88c91fd3067b54ec7a17f5b974391c07346b2210070648af host=ec2-18-215-96-22.compute-1.amazonaws.com sslmode=disable"
+	db, err := sql.Open("postgres", strconnection)
 	if err != nil {
 		panic(err.Error())
 	}
