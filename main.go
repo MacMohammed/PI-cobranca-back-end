a4a5c1d814082f2e5207c532c0d4c3a28b87e29f
MacMohammed <macmohammed@gmail.com>
2022-04-13 07:26:03 -0300

    Teste

--- main.go
+++ main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fatec/routes"
+	"fatec/utils"
 	"fmt"
 	"log"
 	"net/http"
 
 	"os"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-// func init() {
-// 	utils.LoadTemplates()
-// }
+func init() {
+	utils.LoadTemplates()
+}
 
 func main() {
 	rtr := mux.NewRouter()
@@ -24,6 +24,7 @@ func main() {
 
 	fmt.Printf("Inicializando servidor na porta %s", port)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
-		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}), handlers.AllowedOrigins([]string{"*"}))(rtr)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	// log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+	// 	handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}), handlers.AllowedOrigins([]string{"*"}))(rtr)))
 }
